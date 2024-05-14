3f32c290d10a1f3c6c798f672ae0449e8ed20f79
kikeando <117472699+kikeando@users.noreply.github.com>
2024-05-14 00:52:30 -0400

    Add files via upload

---
+++ js-rock-paper-scissor.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+
+function getComputerChoice(){
+    var rock = 1;
+    var paper = 2;
+    var scissor = 3;
+     return Math.floor(Math.random()* 3) + 1;
+    }
+    console.log(getComputerChoice())
+
+function getHumanChoice(){
+    prompt(" Wanna play a game?")
+}
+console.log(getHumanChoice( ))
